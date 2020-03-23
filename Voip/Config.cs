4d60cdc4dfd9b3331ec82fb1dbc649b7e30df4a7
yixin <yixinin@outlook.com>
2020-03-23 22:42:19 +0800

    update. config

--- Voip/Config.cs
+++ Voip/Config.cs
@@ -8,8 +8,8 @@ namespace Voip
 {
     public class Config
     {
-        //public static string HOST = "127.0.0.1";
-        public static string HOST = "10.0.0.218";
+        public static string HOST = "127.0.0.1";
+        //public static string HOST = "10.0.0.218";
         public static short HTTP_PORT = 8080;
         public static short TCP_PORT = 8180;
         public static string MSG_ROUTE = "/livechat/msg";
